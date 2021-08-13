36df39b4e16c26bc9ce7a22622ff2901aef46152
cydal <cydalsij@outlook.com>
2021-08-13 01:24:55 +0100

    fix shape error

--- results.py
+++ results.py
@@ -1,12 +1,13 @@
 from util.plot_utils import plot_logs
 from pathlib import Path
 from PIL import Image
-from utils import *
 import torchvision.transforms as T
 import torch
 import matplotlib.pyplot as plt
 
 
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
 
 # for output bounding box post-processing
 def box_cxcywh_to_xyxy(x):
@@ -24,20 +25,21 @@ def rescale_bboxes(out_bbox, size):
 
 def filter_bboxes_from_outputs(im, outputs,
                                threshold=0.7):
-  
+
   # keep only predictions with confidence above threshold
   probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
   keep = probas.max(-1).values > threshold
 
   probas_to_keep = probas[keep]
 
-  # convert boxes from [0; 1] to image scales
-  bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.size)
+  # convert boxes from [0; 1] to image scales img
+  bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.shape[2:])
   
   return probas_to_keep, bboxes_scaled
 
 
 def run_worflow(my_image, my_model, transform):
+  
   # mean-std normalize the input image (batch-size: 1)
   img = transform(my_image).unsqueeze(0)
 
@@ -126,7 +128,7 @@ if __name__ == "__main__":
 
 
     img_names = ['/content/data/custom/val2017/3825919971_93fb1ec581_b.jpg', 
-                '/content/data/custom/val2017/6810773040_3d81036d05_k.jpg'
+                '/content/data/custom/val2017/6810773040_3d81036d05_k.jpg',
                 '/content/data/custom/val2017/4838031651_3e7b5ea5c7_b.jpg']
 
     for img in img_names:
